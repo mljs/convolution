6b188e01541587cacbe5809a81a61b8582cc4b6a
Daniel Kostro <kostro.d@gmail.com>
2019-04-03 09:26:14 +0200

    export BorderType

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import directConvolution from './directConvolution';
 import fftConvolution from './fftConvolution';
 
 export { directConvolution, fftConvolution };
+export const BorderType = {
+  CONSTANT: 'CONSTANT',
+  CUT: 'CUT'
+};
